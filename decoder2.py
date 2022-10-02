d55fa22cfef84c5f1ec8a7e4d6b54a446e6f0a0f
gadhalevy <gad3dh@gmail.com>
2022-10-02 17:22:18 +0300

    Update decoder2.py

--- decoder2.py
+++ decoder2.py
@@ -21,13 +21,13 @@ def init():
     except ValueError:
         pass
     cred = credentials.Certificate('https://github.com/gadhalevy/verifier/blob/master/mykey.json')
-    tmp = platform.platform()
-    if 'Windows' in tmp:
-        cred = credentials.Certificate("H:/Gibui260318/pythonStuff/verifier/mykey.json")
-        ocr.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    elif 'Linux' in tmp:
-        cred = credentials.Certificate("/media/cimlab/Transcend/Gibui260318/pythonStuff/verifier/mykey.json")
-        ocr.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+#     tmp = platform.platform()
+#     if 'Windows' in tmp:
+#         cred = credentials.Certificate("H:/Gibui260318/pythonStuff/verifier/mykey.json")
+#         ocr.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#     elif 'Linux' in tmp:
+#         cred = credentials.Certificate("/media/cimlab/Transcend/Gibui260318/pythonStuff/verifier/mykey.json")
+#         ocr.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
     firebase_admin.initialize_app(cred, {'databaseURL': 'https://Lab9-c9743.firebaseio.com/',
                                          'storageBucket' :'lab9-c9743.appspot.com'})
 @st.cache(allow_output_mutation=True)
