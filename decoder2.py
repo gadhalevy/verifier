f0a7fc495c51d55a8d96546eaa995c58dc32ac2e
gadhalevy <gad3dh@gmail.com>
2022-10-02 17:31:42 +0300

    Update decoder2.py

--- decoder2.py
+++ decoder2.py
@@ -20,7 +20,7 @@ def init():
         firebase_admin.delete_app(firebase_admin.get_app())
     except ValueError:
         pass
-    cred = credentials.Certificate('https://github.com/gadhalevy/verifier/blob/master/mykey.json')
+    cred = credentials.Certificate('https://raw.githubusercontent.com/gadhalevy/verifier/blob/master/mykey.json')
 #     tmp = platform.platform()
 #     if 'Windows' in tmp:
 #         cred = credentials.Certificate("H:/Gibui260318/pythonStuff/verifier/mykey.json")
