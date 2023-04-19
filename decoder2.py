fc9f74bdba37a6bc901a9f652126067f78722c9b
gadhalevy <gad3dh@gmail.com>
2023-04-19 14:15:46 +0300

    Update decoder2.py

--- decoder2.py
+++ decoder2.py
@@ -1,5 +1,5 @@
 # import firebase_admin
-# import streamlit as st
+import streamlit as st
 # from firebase_admin import credentials
 # from firebase_admin import db
 # from firebase_admin import storage
