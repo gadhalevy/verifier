357101ddbf0851bd0deebbb829cca7a71ea52dbf
gadhalevy <gad3dh@gmail.com>
2023-12-18 14:44:43 +0200

    Update verifier11.py

--- verifier11.py
+++ verifier11.py
@@ -239,7 +239,7 @@ def upload(kind,obj,ref):
         err_code='Must be one of py,kv,txt,nlogo or csv files'
     for c in obj:
         pre,post=c.name.split('.')
-        st.write(c,pre,post)
+        st.write(c.name,pre,post)
         if pre.isdigit():
             if post.lower() in siomet:
                 load(kind, c, *ref[:-1])
