d82ff9662341b880d908d748cf23dd4370507a7a
gadhalevy <gad3dh@gmail.com>
2024-01-14 13:41:38 +0200

    Update verifier11.py

--- verifier11.py
+++ verifier11.py
@@ -253,7 +253,7 @@ def upload(kind,obj,ref):
         pre,post=c.name.split('.')
         if c.name.lower()[-3:] in siomet and pre.isdigit():
             load(kind, c, *ref[:-1])
-            fname=c.name.replace('.','_')
+            fname=c.name.replace('.','-')
             new_ref = ('set',)+ref[:-1] + (kind,fname)
             fbwrite(*new_ref, **{pre: datetime.now(pytz.timezone('Asia/Jerusalem')).strftime('%d-%m-%y %H:%M')})
         else:
