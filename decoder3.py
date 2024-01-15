1545adc0825f55e61d6987ed67dc093d9a6da99d
gadhalevy <gad3dh@gmail.com>
2024-01-15 16:20:33 +0200

    Update decoder3.py

--- decoder3.py
+++ decoder3.py
@@ -225,6 +225,6 @@ def main():
         #     st.markdown(txt)
         if st.sidebar.button('Download grades.csv?'):
             df = pd.read_csv('grades.csv')
-            downloaded=df.to_csv('grades.csv').encode('utf-8')
+            downloaded=df.to_csv('grades.csv',encoding='utf-8',sep=',')
             st.sidebar.download_button(label='Download Grades',data=downloaded, file_name='grades.csv', mime='text/csv')
 main()
