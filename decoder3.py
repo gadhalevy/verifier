0dde7275e6dfe5f9701f46a70498c198a60a796e
gadhalevy <gad3dh@gmail.com>
2024-01-28 13:56:35 +0200

    Update decoder3.py

--- decoder3.py
+++ decoder3.py
@@ -242,7 +242,6 @@ def main():
             st.dataframe(groups)
         if st.sidebar.button('Compare codes?'):
             for f in os.listdir(f'code/{maabada}'):
-                f
                 if f.endswith('py'):
                     with open(f'code/{maabada}/{f}') as data:
                         st.download_button(label=f'{f}',data=data,file_name=f'{f}',mime='text/plain')
