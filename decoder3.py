70bb154dfcc47be33bdc17700fd47bcf92599fcc
gadhalevy <gad3dh@gmail.com>
2024-01-29 14:04:28 +0200

    Update decoder3.py

--- decoder3.py
+++ decoder3.py
@@ -264,15 +264,7 @@ def main():
                 suspects[i]=lst
             for k,v in suspects.items():
                 st.write(f'**:red[Suspected files {" ".join(v)}]**')
-
-
-
-
-
-
-
-
-
+                st.download_button(label=f'Download {k}?',data=dic[k],file_name=k,mime='text/py')
         if st.sidebar.button('Summarize Lab?'):
             txt,flag=not_make_maabada(movies,maabada)
             st.markdown(txt)
