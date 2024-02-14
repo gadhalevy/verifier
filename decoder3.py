d1bf673ee1ceb07956c97460c7db69f26738023f
gadhalevy <gad3dh@gmail.com>
2024-02-14 12:51:47 +0200

    Update decoder3.py

--- decoder3.py
+++ decoder3.py
@@ -224,7 +224,8 @@ def show_missings(year,semester,maabada):
     # tmp = pd.read_csv('grades.csv', index_col=False)
     # df=tmp[['Group members','num']]
     ref=db.reference(f'{year}/{semester}/{maabada}')
-    pd.json_normalize(ref.get())
+    tmp=pd.json_normalize(ref.get(),record_path='missing')
+    st.write(tmp)
 
 
 
